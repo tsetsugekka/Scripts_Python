8140a0de2b790a277ad48f69fa1267f367c28c16
tóng <kanagawa00@gmail.com>
2021-01-07 15:23:48 +0900

    Create TwilioSMSお知らせ.py

---
+++ TwilioSMSお知らせ.py
@@ -0,0 +1,14 @@
+#SMSで完了をお知らせ 
+# Your Account SID from twilio.com/console 
+account_sid = "################" 
+# Your Auth Token from twilio.com/console 
+auth_token  = "###############" 
+ 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create( 
+    to="+81#######3",  
+    from_="+16#######1", 
+    body="スクレピング完了しました。"+filename+"を確認ください。") 
+ 
+print(message.sid) 
