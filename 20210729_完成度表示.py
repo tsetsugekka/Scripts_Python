0ac9304b785f91e35197458500a451a4402158f5
tóng <kanagawa00@gmail.com>
2021-07-29 13:53:15 +0900

    Create 20210729_完成度表示.py

---
+++ 20210729_完成度表示.py
@@ -0,0 +1,15 @@
+import datetime
+
+printnum=0
+for index,row in df.iterrows():
+    #打印进度
+    #A：逢10打印
+    if (index+1)%10==0:
+        print("\r"+datetime.datetime.now().isoformat(timespec='minutes')+"　　"+str(index+1)+"件目実行開始")
+    
+    #B：全部打印
+    #考虑到index存在不从0开始且筛选后不连续的情况，
+    #所以计算完成度时使用index而使用printnum
+    printnum=printnum+1
+    print("\r"+datetime.datetime.now().isoformat(timespec='minutes')+"　　"+str(index+1)+"件目実行開始（完成度："+str(round(printnum/len(df)*100,2))+"%）　　　　　　　　", end="")
+   
